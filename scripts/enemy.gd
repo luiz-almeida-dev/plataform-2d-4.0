d662d859fcb37e07edfaa9a60d23f8f871eb0b94
Luiz Almeida <146768585+luiz-almeida-dev@users.noreply.github.com>
2025-10-17 13:49:43 -0300

    feat(world): add hit and hurt box

---
+++ scripts/enemy.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+
+const SPEED = 700.0
+const JUMP_VELOCITY = -400.0
+
+var direction := -1 
+@onready var wall_detector := $wall_detector as RayCast2D
+@onready var texture := $texture as Sprite2D
+@onready var anim := $anim as AnimationPlayer
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if wall_detector.is_colliding():
+		direction *= -1
+		wall_detector.scale.x *= -1
+		
+	if direction == 1:
+		texture.flip_h = true
+	else:
+		texture.flip_h = false
+
+	velocity.x = direction * SPEED * delta
+
+	move_and_slide()
+
+
+func _on_anim_animation_finished(anim_name: StringName) -> void:
+	if anim_name == 'hurt':
+		queue_free()
+	
