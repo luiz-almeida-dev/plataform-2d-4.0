165760cbea81ca824597601877cea78f9c448c8d
Luiz-1213 <146768585+luiz-almeida-dev@users.noreply.github.com>
2025-10-20 08:05:13 -0300

    feat(player): add knockback efect on player damage

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,18 @@
 extends CharacterBody2D
 
-
 const SPEED = 200.0
 const JUMP_FORCE = -400.0
 
 var is_jumping := false
+var is_hurted := false
+var knockback_vector := Vector2.ZERO
+var direction
+@export var player_life := 10
+
 @onready var animation:= $anim as AnimatedSprite2D
 @onready var remote_transform := $remote as RemoteTransform2D
+@onready var ray_right := $ray_right as RayCast2D
+@onready var ray_left := $ray_left as RayCast2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -20,28 +26,66 @@ func _physics_process(delta: float) -> void:
 	elif is_on_floor():
 		is_jumping= false
 		
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+		 
+	direction = Input.get_axis("ui_left", "ui_right")
+	
 	if direction:
 		velocity.x = direction * SPEED
 		animation.scale.x = direction
-		if !is_jumping:
-			animation.play("run")
-	elif is_jumping:
-		animation.play("jump")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		animation.play("idle")
 
+	if knockback_vector != Vector2.ZERO:
+		velocity = knockback_vector
+	
+	_set_state()
 	move_and_slide()
 
 
 func _on_hurtbox_body_entered(body: Node2D) -> void:
-	if body.is_in_group('enemies'):
+	if player_life < 0:
 		queue_free()
+	else:
+		if(ray_right.is_colliding()):
+			take_damage(Vector2(-200, -200))
+		elif(ray_left.is_colliding()):
+			take_damage(Vector2(200, -200))
 		
 func follow_camera(camera):
 	var camera_path = camera.get_path()
 	remote_transform.remote_path = camera_path
+
+func take_damage(knockback_force:= Vector2.ZERO, duration := 0.25):
+	player_life -= 1
+	
+	if  knockback_force != Vector2.ZERO:
+		knockback_vector = knockback_force
+		
+		var knockback_tween := get_tree().create_tween()
+		knockback_tween.parallel().tween_property(self, "knockback_vector", Vector2.ZERO, duration)
+		animation.modulate = Color(1,0,0,1)
+		knockback_tween.parallel().tween_property(animation, "modulate", Color(1,1,1,1), duration)
+	
+	is_hurted = true
+	await get_tree().create_timer(.3).timeout
+	is_hurted = false
+	
+	
+func _set_state():
+	var state = "idle"
+	
+	if !is_on_floor():
+		state = "jump"
+	elif direction != 0:
+		state = "run"
+		
+	if is_hurted:
+		state = "hurt"	
+		
+	if animation.name != state:
+		animation.play(state)
+	
+	
+	
+	
